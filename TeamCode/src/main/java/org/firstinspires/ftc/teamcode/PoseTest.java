1587b5f2af2077545cf01f7170076883b43635f9
richpants <rmansfield@creightonprep.org>
2020-10-17 13:26:59 -0500

    This is a test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseTest.java
@@ -1,6 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 public class PoseTest {
+}
+
+public class MyOpmode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(10)
+                .forward(5)
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectory(myTrajectory);
+    }
 }
 new TrajectoryBuilder(new Pose2d())
         .forward(40)
